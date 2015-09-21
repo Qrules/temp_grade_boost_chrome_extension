6343eae539faf5141915e326b0a2bb8d037de991
Patrick Bell <ext0@users.noreply.github.com>
2015-09-21 12:35:15 -0500

    Create domEdit.js

---
+++ grade_boost/domEdit.js
@@ -0,0 +1,4 @@
+var elements = document.getElementsByClassName("sg-font-larger-average");
+for(var i=0;i<elements.length;i++){
+	elements[i].innerHTML = String((Math.random()*7.00)+93).substring(0,5);
+}
