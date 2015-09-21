f6e31ac6bf15533d2f185b6b473ff827566b5f51
Patrick Bell <ext0@users.noreply.github.com>
2015-09-21 13:00:14 -0500

    Update domEdit.js

--- grade_boost/domEdit.js
+++ grade_boost/domEdit.js
@@ -1,6 +1,6 @@
 var elements = document.getElementsByClassName("sg-font-larger-average"); 
 chrome.storage.sync.get("lowest", function (obj) {
 	for(var i=0;i<elements.length;i++){
-		elements[i].innerHTML = String((Math.random()*(100-obj))+obj).substring(0,5);
+		elements[i].innerHTML = String((Math.random()*(100-Number(obj)))+Number(obj)).substring(0,5);
 	}
 });
