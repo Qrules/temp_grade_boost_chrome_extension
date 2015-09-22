d2de120db71f6e76fdf4356e4a5a158ba2e0b437
Patrick Bell <ext0@users.noreply.github.com>
2015-09-21 19:13:28 -0500

    Cleaned code a bit

--- grade_boost/options.js
+++ grade_boost/options.js
@@ -1,21 +1,12 @@
 function save_options() {
-  var low = 100.0;
-  var error = 1;
-  var temp = 0.0;
-  temp = document.getElementById('l').value;
-  if (temp >= 0 && temp <= 100) {
-    low = temp;
-    error = 0;
-  }
+  var low = document.getElementById('l').value;
+  low = (temp >= 0 && temp <= 100)?low:-1;
   chrome.storage.sync.set({
     lowest: low
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
-    if (error)
-      status.textContent = 'Error, incorrect value!';
-    else
-      status.textContent = 'Options Saved: ' + low + '!';
+    status.textContent = (low!=-1)?'Options Saved: ' + low + '!':'Error, incorrect value!';
     setTimeout(function() {
       status.textContent = '';
     }, 5000);
