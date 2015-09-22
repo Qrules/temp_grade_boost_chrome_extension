d6bfb42806a5146ac417fcb4095b7a25e7e69b81
Patrick Bell <ext0@users.noreply.github.com>
2015-09-22 10:46:54 -0500

    Update background.js

--- grade_boost/background.js
+++ grade_boost/background.js
@@ -1,4 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-//maybe open a page to modify the grade setting?
-//the other code was moved to editDOM.js, and that code will be injected into every page in the https://accesscenter.roundrockisd.org/* namespace
+  chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
 });
