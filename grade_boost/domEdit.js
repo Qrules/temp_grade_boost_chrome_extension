d5ac7f63770dff0dd955c05dc8409372f5d99bad
Patrick Bell <ext0@users.noreply.github.com>
2015-09-21 19:33:26 -0500

    Update domEdit.js

--- grade_boost/domEdit.js
+++ grade_boost/domEdit.js
@@ -1,6 +1,6 @@
 var elements = document.getElementsByClassName("sg-font-larger-average"); 
 chrome.storage.sync.get("lowest", function (obj) {
 	for(var i=0;i<elements.length;i++){
-		elements[i].innerHTML = String((Math.random()*(100-Number(obj)))+Number(obj)).substring(0,5);
+		elements[i].innerHTML = String(Number((Math.random()*(100.00-obj.lowest)))+Number(obj.lowest)).substring(0,5); //wow this was a pain to get working, apparentally + is concatanation when dealing with two numbers. Who would've thought?
 	}
 });
