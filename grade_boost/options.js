29fab574da1da3961e84f33b6a69753b51fec70b
Qrules <Qrules@users.noreply.github.com>
2015-09-23 15:18:02 -0500

    Fixed options storage

--- grade_boost/options.js
+++ grade_boost/options.js
@@ -1,8 +1,10 @@
 function save_options() {
   var low = document.getElementById('l').value;
   low = (low >= 0 && low <= 100)?low:-1;
+  configBool = 0;
   chrome.storage.sync.set({
-    lowest: low
+  configured: configBool  
+  lowest: low
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -13,7 +15,15 @@ function save_options() {
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
+// Restores score stored in chrome.storage.
+function restore_options() {
+  // Use default value color of 100.
+  chrome.storage.sync.get({
+    lowest: 100
+  }, function(items) {
+    document.getElementById('save').value = items.lowest;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
